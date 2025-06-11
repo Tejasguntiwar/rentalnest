b9c074074f999989a587354c67e7e7883e98b532
Tejasguntiwar2 <tejas.22420114@viit.ac.in>
2025-06-11 19:31:49 +0530

    Added Boilerplate and public/css/style.css

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -10,6 +11,8 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
