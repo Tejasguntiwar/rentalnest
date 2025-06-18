5f21bfd1977fb9b2f8ab627598af19e2f2f44f67
Tejasguntiwar2 <tejas.22420114@viit.ac.in>
2025-06-18 17:53:52 +0530

    Created / Separated  Routes for /listings and /listings/:id/reviews in /routes/listing.js and /routes/review.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema, reviewSchema } = require("./schema.js");
-const Review = require("./models/review.js");
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -36,125 +35,14 @@ app.get("/", (req, res) => {
   res.send("This is Root");
 });
 
-const validateListing = (req, res, next) => {
-  let {error} = listingSchema.validate(req.body);
-  if (error) {
-    let errorMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errorMsg);
-  } else {
-    next();
-  }
-};
-
-const validateReview = (req, res, next) => {
-  let {error} = reviewSchema.validate(req.body);
-  if (error) {
-    let errorMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errorMsg);
-  } else {
-    next();
-  }
-};
 
 
-//Index Route
-app.get(
-  "/listings",
-  wrapAsync(async (req, res) => {
-    let allListings = await Listing.find({});
-    res.render("listings/index.ejs", { allListings });
-  })
-);
 
-//New Route
-app.get(
-  "/listings/new",
-  wrapAsync(async (req, res) => {
-    res.render("listings/new.ejs");
-  })
-);
+//All the routes related to Listings (/listings are moved) to separate /routes/listing.js file
+app.use("/listings", listings);
 
-//Show Route
-app.get(
-  "/listings/:id",
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs", { listing });
-  })
-);
-
-//Create Route
-app.post(
-  "/listings", validateListing,
-  wrapAsync(async (req, res, next) => {
-    // let {title, description, image, price, country, location} = req.body;
-    // let newListing = req.body.listing;
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-  })
-);
-
-//Edit Route
-app.get(
-  "/listings/:id/edit",
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-
-    res.render("listings/edit.ejs", { listing });
-  })
-);
-
-//Update Route
-app.put(
-  "/listings/:id", validateListing,
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-
-    const listing = await Listing.findById(id); // ✅ Get updated listing
-
-    res.render("listings/show.ejs", { listing });
-  })
-);
-
-//DELETE Route
-app.delete(
-  "/listings/:id",
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-
-    await Listing.findByIdAndDelete(id);
-
-    res.redirect("/listings");
-  })
-);
-
-
-//Reviews Post Route
-app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res, next) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-    
-    res.redirect(`/listings/${listing._id}`);
-  }));
-
-//Delete Reviews Route
-app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
-    let {id, reviewId} = req.params;
-    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-
-    res.redirect(`/listings/${id}`);
-}));
+//All the routes related to Reviews (/listings/:id/review) are moved to separate /routes/review.js file
+app.use("/listings/:id/reviews", reviews);
 
 
 // app.get("/testListing", async (req, res) => {
