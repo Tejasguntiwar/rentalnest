829839ba338f2ddc1e5e927acd25557d7c20661d
Tejasguntiwar2 <tejas.22420114@viit.ac.in>
2025-06-19 10:57:52 +0530

    Added express-session and connect-flash for Session management and Proper Error or Successs Msgs

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
@@ -17,15 +19,29 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+
+const sessionOptions = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true
+  }
+};
+
+
+
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-  .then(() => {
-    console.log("Connection successfull..");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+.then(() => {
+  console.log("Connection successfull..");
+})
+.catch((err) => {
+  console.log(err);
+});
 
 async function main() {
   await mongoose.connect(MONGO_URL);
@@ -36,6 +52,15 @@ app.get("/", (req, res) => {
 });
 
 
+app.use(session(sessionOptions));
+app.use(flash());
+
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+})
 
 
 //All the routes related to Listings (/listings are moved) to separate /routes/listing.js file
--- routes/listing.js
+++ routes/listing.js
@@ -40,6 +40,13 @@ router.get(
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+
+    if(!listing) {
+      req.flash("error", "Listing you requested for does not exist!");
+      res.redirect("/listings");
+      return;
+    }
+
     res.render("listings/show.ejs", { listing });
   })
 );
@@ -54,6 +61,7 @@ router.post(
     // let newListing = req.body.listing;
     const newListing = new Listing(req.body.listing);
     await newListing.save();
+    req.flash("success", "New Listing Created!");
     res.redirect("/listings");
   })
 );
@@ -64,6 +72,12 @@ router.get(
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+
+    if(!listing) {
+      req.flash("error", "Listing you requested for does not exist!");
+      res.redirect("/listings");
+      return;
+    }
 
     res.render("listings/edit.ejs", { listing });
   })
@@ -79,7 +93,9 @@ router.put(
 
     const listing = await Listing.findById(id); // ✅ Get updated listing
 
-    res.render("listings/show.ejs", { listing });
+    req.flash("success", "Listing Updated!");
+
+    res.redirect(`/listings/${id}`);
   })
 );
 
@@ -91,6 +107,7 @@ router.delete(
 
     await Listing.findByIdAndDelete(id);
 
+    req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
   })
 );
